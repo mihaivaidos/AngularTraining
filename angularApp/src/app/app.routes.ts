fcb488a56c1f30f877a8a73dbb07de0a1f50a497
mihaivaidos <mihai@vaidos.com>
2025-11-26 11:58:23 +0200

    Update app config and app routes to use DogComponent

--- angularApp/src/app/app.routes.ts
+++ angularApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {DogComponent} from './dogFetcher/components/dog/dog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DogComponent
+  }
+];
