e19a39428694ef7dca2efa6e881ea6f2cbacd165
mihaivaidos <mihai@vaidos.com>
2025-11-27 15:00:47 +0200

    Add admin page and protect the route with guard

--- angularApp/src/app/app.routes.ts
+++ angularApp/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from './home.component';
-import {NotFoundComponent} from './not-found.component';
-import {authGuard} from './auth.guard';
+import {HomeComponent} from './features/home/home.component';
+import {NotFoundComponent} from './features/not-found/not-found.component';
+import {authGuard} from './core/guards/auth.guard';
+import {AdminPageComponent} from './features/admin-page/admin-page.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
-  { path: 'login', loadComponent: () => import('./auth/login.component').then(m => m.LoginComponent) },
+  { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminPageComponent, canActivate: [authGuard]},
+  { path: 'login', loadComponent: () => import('./features/auth/login.component').then(m => m.LoginComponent) },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', component: NotFoundComponent },
 ];
